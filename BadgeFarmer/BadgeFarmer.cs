caa71d4b2fb3f82d23ec1f8cf7f9019752355938
Ilin Kirill <ilin_k@protei.ru>
2020-07-09 21:59:35 +0300

    Initial commit

---
+++ BadgeFarmer/BadgeFarmer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BadgeFarmer
+{
+    [Export(typeof(IPlugin))]
+    public class BadgeFarmer : IBotSteamClient, IBotCommand {
+        public string Name => "Badge farmer";
+        public Version Version => new Version(0, 1);
+
+        public void OnLoaded() {
+            Console.WriteLine("a");
+        }
+
+        public Task<string> OnBotCommand(Bot bot, ulong steamID, string message, string[] args) => throw new NotImplementedException();
+
+        public void OnBotSteamCallbacksInit(Bot bot, CallbackManager callbackManager) { }
+
+        public IReadOnlyCollection<ClientMsgHandler> OnBotSteamHandlersInit(Bot bot) {
+            return new ClientMsgHandler[0];
+        }
+    }
+}
